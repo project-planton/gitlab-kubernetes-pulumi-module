74c200f53be3d37dc950b9d3ef49c6f59931d8dd
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-12 16:53:38 +0530

    moved to project-planton

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	gitlabkubernetesv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/kubernetes/gitlabkubernetes/v1"
+	gitlabkubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/gitlabkubernetes/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/gitlab-kubernetes-pulumi-module/pkg"
-	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
+	"github.com/project-planton/gitlab-kubernetes-pulumi-module/pkg"
+	"github.com/project-planton/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
