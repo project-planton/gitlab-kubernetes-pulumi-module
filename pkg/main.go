5443b92e92bfd055fe2bce5baa45d6e40be7d8cc
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-11-09 17:58:34 +0530

    switched over to using local generated stubs for project-planton apis

--- pkg/main.go
+++ pkg/main.go
@@ -1,9 +1,9 @@
 package pkg
 
 import (
-	gitlabkubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/gitlabkubernetes/v1"
 	"github.com/pkg/errors"
-	"github.com/project-planton/pulumi-module-golang-commons/pkg/provider/kubernetes/pulumikubernetesprovider"
+	gitlabkubernetesv1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/kubernetes/gitlabkubernetes/v1"
+	"github.com/project-planton/project-planton/pkg/pulmod/provider/kubernetes/pulumikubernetesprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
